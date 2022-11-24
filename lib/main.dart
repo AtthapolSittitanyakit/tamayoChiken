fb043e5f417f91ea94869a3b974dcf9d7476a55c
Atthapol_Sittitanyakit <thekop.forever.mbs@gmail.com>
2022-11-24 02:49:30 +0000

    Create Project

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,89 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'ExchangeRate.dart';
+import 'MoneyBox.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      title: "My App",
+      home: MyHomePage(),
+      theme: ThemeData(primarySwatch: Colors.purple),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  ExchangeRate _dataFromAPI;
+  @override
+  void initState() {
+    super.initState();
+    getExchangeRate();
+  }
+
+  Future<ExchangeRate> getExchangeRate() async {
+    var url = "https://api.exchangeratesapi.io/latest?base=THB";
+    var response = await http.get(url);
+    _dataFromAPI = exchangeRateFromJson(response.body); //json => dart object
+    return _dataFromAPI;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+        appBar: AppBar(
+          title: Text("แปลงสกุลเงิน",
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
         ),
-      ),
-    );
+        body: FutureBuilder(
+          future: getExchangeRate(),
+          builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+            //ดึงข้อมูลจาก getExchangeRate มาครบเรียบร้อยจะให้ทำอะไร
+            if (snapshot.connectionState == ConnectionState.done) {
+              var result = snapshot.data;
+              double amount = 10000;
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    MoneyBox("สกุลเงิน (THB)", amount, Colors.lightBlue, 150),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    MoneyBox(
+                        "USD", amount * result.rates["USD"], Colors.green, 100),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    MoneyBox(
+                        "EUR", amount * result.rates["EUR"], Colors.red, 100),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    MoneyBox(
+                        "GBP", amount * result.rates["GBP"], Colors.pink, 100),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    MoneyBox("JPY", amount * result.rates["JPY"], Colors.orange,
+                        100),
+                  ],
+                ),
+              );
+            }
+            return LinearProgressIndicator();
+          },
+        ));
   }
 }
